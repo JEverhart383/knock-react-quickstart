7dc233e3c772529e5193756ba6ce41baf513f140
Jeff Everhart <jeffeverhart383@gmail.com>
2025-08-28 15:04:49 -0400

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Inbox from "./components/inbox";
+
+function App() {
+  return (
+    <>
+      <h1>Knock Inbox Test</h1>
+      <Inbox></Inbox>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/inbox.tsx
@@ -0,0 +1,39 @@
+import {
+  KnockProvider,
+  KnockFeedProvider,
+  NotificationFeed,
+  NotificationIconButton,
+} from "@knocklabs/react";
+
+// Required CSS import, unless you're overriding the styling
+import "@knocklabs/react/dist/index.css";
+
+const Inbox = () => {
+  // An example of fetching the current authenticated user
+  const user = {
+    id: "12c23775-5902-481a-b8ea-d1704aabc769",
+  };
+
+  return (
+    <KnockProvider
+      apiKey={import.meta.env.VITE_KNOCK_PUBLIC_API_KEY}
+      user={{ id: user.id }}
+    >
+      <KnockFeedProvider
+        feedId={import.meta.env.VITE_KNOCK_FEED_CHANNEL_ID}
+        colorMode="dark"
+      >
+        <>
+          <div style={{ marginBottom: "1rem" }}>
+            <NotificationIconButton onClick={(e) => {}} />
+          </div>
+          <div style={{ width: "420px", height: "550px" }}>
+            <NotificationFeed />
+          </div>
+        </>
+      </KnockFeedProvider>
+    </KnockProvider>
+  );
+};
+
+export default Inbox;
